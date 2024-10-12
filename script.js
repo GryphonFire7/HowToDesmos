f2ca64ff3b46b8509a079900f5d58b2801aed881
GryphonFire7 <161389723+GryphonFire7@users.noreply.github.com>
2024-10-12 01:49:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const body = document.body
+const getElement = (id) => {
+  return document.getElementById(id)
+}
+const range = (count) => {
+  let ret = []
+  for (let i;i<count;i++) {
+    ret.push(i)
+  }
+  return ret
+}
